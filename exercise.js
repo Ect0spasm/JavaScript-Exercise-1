9d796aa4c789f4a67e4fe622e24a2be562f4ff3c
Ect0spasm <143848554+Ect0spasm@users.noreply.github.com>
2024-01-23 15:12:23 -0800

    Modest

---
+++ exercise.js
@@ -0,0 +1,10 @@
+function makeboard(width,height){
+    let board = "";
+    for(y = 0; y < height; y++){
+        (y+x) % 2 === 0 ? board
+    :
+    board += "/n";
+}
+return board;
+
+console.log(triangle(7));}
