c9fce97c75447f55edb51102d0d06cef410bad64
Ect0spasm <gavin.stetson23@student.cart.org>
2024-01-25 23:29:52 -0800

    Thursday

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let theQuestion = prompt("Enter a number into le calculator");
+const display = document.getElementById("answer");
+
+function calculate(){
+    answer.value += Input;
+    try{
+        display.value = eval(answer.value);
+    }
+    catch(error){
+        answer.value = "This is not the path.";
+    }
+}
+
+/*let theQuestion = prompt("Enter a number into le calculator");
 
 answer = window.prompt("Is this a question?");
-console.log(theAnswer)
-
+console.log(theAnswer)*/
