4f5e94175f61f49a80222c2320c303d3207af7e9
Ect0spasm <gavin.stetson23@student.cart.org>
2024-01-26 00:35:44 -0800

    2.2

--- main.js
+++ main.js
@@ -17,6 +17,17 @@ function clearDisplay(){
     display.value = "";
 }
 
+function press(board) { 
+    if (press.key == '0' || press.key == '1' 
+        || press.key == '2' || press.key == '3' 
+        || press.key == '4' || press.key == '5' 
+        || press.key == '6' || press.key == '7' 
+        || press.key == '8' || press.key == '9' 
+        || press.key == '+' || press.key == '-' 
+        || press.key == '*' || press.key == '/') 
+        document.getElementById("result").value += press.key; 
+} 
+
 /*let theQuestion = prompt("Enter a number into le calculator");
 
 answer = window.prompt("Is this a question?");
